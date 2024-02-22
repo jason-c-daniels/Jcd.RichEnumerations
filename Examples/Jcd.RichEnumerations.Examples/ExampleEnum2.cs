6563031cf534a099b0635ccdf9cc652cd3e32ead
Jason C. Daniels <jason.c.daniels@gmail.com>
2024-02-22 11:35:25 -0600

    - Added lots of example code. - Added ReSharper rules for code formatting. - Reformatted code according to the ReSharper rules. - Added editorconfig rules. -- Added numerous ReSharper warning suppressions, mostly about boxing conversions.

---
+++ Examples/Jcd.RichEnumerations.Examples/ExampleEnum2.cs
@@ -0,0 +1,23 @@
+﻿// ReSharper disable HeapView.ObjectAllocation.Evident
+// ReSharper disable UnusedMember.Global
+
+namespace Jcd.RichEnumerations.Examples;
+
+public class ExampleEnum2(int value, string name, string description)
+   : NamedAndDescribedSortableRichEnum<ExampleEnum2>(value, name, description)
+{
+   #region Enumerated items
+
+   public static readonly ExampleEnum2 Item2 = new(2, "ExampleEnum2.Item2", "The second item.");
+   public static readonly ExampleEnum2 Item1 = new(1, "ExampleEnum2.Item1", "The first item.");
+   public static readonly ExampleEnum2 Unset = new(0, "ExampleEnum2.Unset", "The value is not yet set.");
+
+   #endregion
+
+   /// <summary>
+   /// Compares two instances by their Value property.
+   /// </summary>
+   /// <param name="other">The other instance to compare to.</param>
+   /// <returns>-1 if this instance is less than the other, 1 if this instance is greater than the other. 0 if they are equal.</returns>
+   public override int CompareTo(ExampleEnum2 other) { return Value.CompareTo(other.Value); }
+}
